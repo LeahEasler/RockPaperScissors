cf8de37269ba1cd11ee49103034937e1087b33cb
Leah Easler <leahbeasler@outlook.com>
2024-01-05 06:24:18 -0500

    Rock Paper Scisscors v 1.0
    Completed code.
    
    Issue I had ran into:
    - Indent was wrong after main loop
    - random.randomint was randomit fixed that as well as added a space between , 3.
    - Other keys pressed did not show error.

--- RockPaperScisscor.py
+++ RockPaperScisscor.py
@@ -6,20 +6,55 @@ losses = 0
 ties = 0
 
 while True: #The main loop
-    print("%s Wins, %s Loses, %s Ties" (wins, loses, ties))
-    while True:
-        print("Enter your move: (R)ock (P)aper (S)cisscor or (Q)uit: ")
+    print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
+    while True: # Loop Looks for player input and prints error if wrong key is pressed
+        print("Enter your move: (r)ock (p)aper (s)cisscor or (q)uit: ")
         playerMove = input()
-        if playerMove == 'q'
+        if playerMove == 'q':
             sys.exit()
-        if playerMove == 'p' if playerMove == 'r' if playerMove == 's':
+        if playerMove == 'p' or playerMove == 'r' or playerMove == 's':
             break #leaves main loop
-        print('Type one of the r, p, s or q.')
+        else:
+            print('Type one of the r, p, s or q.')
 
-        #Player choice displayed:
-        if playerMove == 'r':
-            print('Rock versus...')
-        elif playerMove == 'p':
+        # Player choice displayed:
+    if playerMove == 'r':
+        print('Rock versus...')
+    elif playerMove == 'p':
             print('Paper versus...')
-        elif playerMove == 's':
+    elif playerMove == 's':
             print('Scisscors versus...')
+
+    randomNumber = random.randint(1, 3) # Defines random number and prints computer move
+    if randomNumber == 1:
+       computerMove = 'r'
+       print('ROCK')
+    elif randomNumber == 2:
+       computerMove = 'p'
+       print('PAPER')
+    elif randomNumber == 3:
+       computerMove = 's'
+       print('SCISSCORS')
+
+        # Checking result and counting the Wins, Loses, and Ties
+    if playerMove == computerMove:
+        print('You tied with the computer!')
+        ties = ties + 1 # Adds to ties counter
+    elif playerMove == 'r' and computerMove == 's':
+        print('YOU WIN!')
+        wins = wins + 1 # Adds to wins
+    elif playerMove == 'p' and computerMove == 'r':
+        print('YOU WIN!')
+        wins = wins + 1
+    elif playerMove == 's' and computerMove == 'p':
+        print('YOU WIN!')
+        wins = wins + 1
+    elif playerMove == 'r' and computerMove == 'p':
+        print('Better luck next time! You lose...')
+        losses = losses + 1
+    elif playerMove == 's' and computerMove == 'r':
+        print('Better luck next time! You lose...')
+        losses = losses + 1
+    elif playerMove == 'p' and computerMove == 's':
+        print('Better luck next time! You lose...')
+        losses = losses + 1
